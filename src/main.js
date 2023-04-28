73af8a91d6fa599e2217ab25126805f0f1644572
Pronin Maks <maksimpronin1991@icloud.com>
2023-04-28 21:52:48 +0300

    add view presenter

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import FilterView from './view/filter-view.js';
 import TripInfo from './view/trip-info-view.js';
-import {render} from './render.js';
 import BoarderPresenter from './presenter/board-presenter.js';
+import {render} from './render.js';
 
 const tripMain = document.querySelector('.trip-main');
 const controlsFilters = document.querySelector('.trip-controls__filters');
 const tripEvents = document.querySelector('.trip-events');
 
-const boarderPresenter = new BoarderPresenter({boardContainer: tripEvents});
-
 render(new TripInfo(),tripMain,'afterbegin');
 render(new FilterView(), controlsFilters);
 
+const boarderPresenter = new BoarderPresenter({container:tripEvents});
 boarderPresenter.init();
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,8 +1,8 @@
-import SortView from './view/sort-view.js';
-import TripEventList from './view/waypoint-list-view.js';
-import CreateFormView from './view/create-form-view.js';
-import TripEventItem from './view/waipoint-item-view.js';
-import {render} from './render.js';
+import SortView from '../view/sort-view.js';
+import TripEventList from '../view/waypoint-list-view.js';
+import CreateFormView from '../view/create-form-view.js';
+import TripEventItem from '../view/waipoint-item-view.js';
+import {render} from '../render.js';
 
 export default class BoarderPresenter {
   sortComponent = new SortView();
@@ -23,3 +23,4 @@ export default class BoarderPresenter {
     }
   }
 }
+
