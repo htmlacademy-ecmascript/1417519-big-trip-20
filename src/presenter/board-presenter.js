1acf7450ab7352320c22b4a803eb204465b3540f
Pronin Maks <maksimpronin1991@icloud.com>
2023-05-09 18:10:14 +0300

    maked a bit of fields private in board and model

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -5,27 +5,35 @@ import TripEventItem from '../view/waipoint-item-view.js';
 import { render } from '../framework/render.js';
 
 export default class BoarderPresenter {
-  sortComponent = new SortView();
-  eventListComponent = new TripEventList();
+  #container = null;
+  #pointsModel = null;
+
+  #sortComponent = new SortView();
+  #eventListComponent = new TripEventList();
+
+
+  #boardPoints = [];
+  #pointsOffers = [];
+  #pointsDestinations = [];
 
   constructor({container,pointsModel}){
-    this.container = container;
-    this.pointsModel = pointsModel;
+    this.#container = container;
+    this.#pointsModel = pointsModel;
   }
 
   init(){
-    this.boardPoints = [...this.pointsModel.getPoints()];
-    this.pointsOffers = [...this.pointsModel.getOffers()];
-    this.pointsDestinations = [...this.pointsModel.getDestinations()];
+    this.#boardPoints = [...this.#pointsModel.points];
+    this.#pointsOffers = [...this.#pointsModel.offers];
+    this.#pointsDestinations = [...this.#pointsModel.destinations];
 
-    render(this.sortComponent,this.container);
-    render(this.eventListComponent,this.container);
-    render(new CreateFormView({point: this.boardPoints[0], offer: this.pointsOffers,
-      destination:this.pointsDestinations}),this.eventListComponent.element);
+    render(this.#sortComponent,this.#container);
+    render(this.#eventListComponent,this.#container);
+    render(new CreateFormView({point: this.#boardPoints[0], offer: this.#pointsOffers,
+      destination:this.#pointsDestinations}),this.#eventListComponent.element);
 
-    for(let i = 0; i < this.boardPoints.length; i++){
-      render(new TripEventItem({point: this.boardPoints[i],offer: this.pointsOffers,
-        destination:this.pointsDestinations}),this.eventListComponent.element);
+    for(let i = 0; i < this.#boardPoints.length; i++){
+      render(new TripEventItem({point: this.#boardPoints[i],offer: this.#pointsOffers,
+        destination:this.#pointsDestinations}),this.#eventListComponent.element);
     }
   }
 }
